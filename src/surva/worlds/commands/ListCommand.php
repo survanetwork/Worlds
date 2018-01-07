72c6c05e4d36abbdc22204f2a385d5066b3cabe8
Jarne <jarne@users.noreply.github.com>
2018-01-07 12:48:30 +0100

    Added unloaded levels to the list command

--- src/surva/worlds/commands/ListCommand.php
+++ src/surva/worlds/commands/ListCommand.php
@@ -9,13 +9,26 @@
 namespace surva\worlds\commands;
 
 use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 
 class ListCommand extends CustomCommand {
     public function do(Player $player, array $args) {
         $levels = array();
 
         foreach($this->getWorlds()->getServer()->getLevels() as $level) {
-            $levels[] = $level->getName();
+            $levels[] = TextFormat::WHITE . $level->getName();
+        }
+
+        $worldsPath = $this->getWorlds()->getServer()->getDataPath() . "worlds";
+
+        foreach(scandir($worldsPath) as $unloadedLevel) {
+            if(
+                is_dir($worldsPath . "/" . $unloadedLevel) AND
+                is_file($worldsPath . "/" . $unloadedLevel . "/level.dat") AND
+                !in_array(TextFormat::WHITE . $unloadedLevel, $levels)
+            ) {
+                $levels[] = TextFormat::GRAY . $unloadedLevel;
+            }
         }
 
         $player->sendMessage($this->getWorlds()->getMessage("list.worlds", array("worlds" => implode(", ", $levels))));
