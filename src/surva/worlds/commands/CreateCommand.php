b05f6412d5a6ebbf030236164763884105566102
Jarne <dev@jarne.rocks>
2021-04-08 20:05:52 +0200

    Changed supported level generators to PocketMine official API

--- src/surva/worlds/commands/CreateCommand.php
+++ src/surva/worlds/commands/CreateCommand.php
@@ -6,9 +6,7 @@
 namespace surva\worlds\commands;
 
 use pocketmine\command\CommandSender;
-use pocketmine\level\generator\Flat;
-use pocketmine\level\generator\hell\Nether;
-use pocketmine\level\generator\normal\Normal;
+use pocketmine\level\generator\GeneratorManager;
 
 class CreateCommand extends CustomCommand {
     public function do(CommandSender $sender, array $args): bool {
@@ -16,33 +14,28 @@ class CreateCommand extends CustomCommand {
             case 1:
                 if(!$this->getWorlds()->getServer()->generateLevel($args[0])) {
                     $sender->sendMessage($this->getWorlds()->getMessage("create.failed"));
+
+                    return true;
                 }
 
                 $sender->sendMessage($this->getWorlds()->getMessage("create.success", array("name" => $args[0])));
 
                 return true;
             case 2:
-                switch(strtolower($args[1])) {
-                    case "normal":
-                        $generator = Normal::class;
-                        break;
-                    case "flat":
-                        $generator = Flat::class;
-                        break;
-                    case "nether":
-                        $generator = Nether::class;
-                        break;
-                    default:
-                        $sender->sendMessage(
-                            $this->getWorlds()->getMessage("create.generator.notexist", array("name" => $args[1]))
-                        );
+                $givenGenName = strtolower($args[1]);
 
-                        $generator = Normal::class;
-                        break;
+                if(!in_array($givenGenName, GeneratorManager::getGeneratorList())) {
+                    $sender->sendMessage(
+                        $this->getWorlds()->getMessage("create.generator.notexist", array("name" => $args[1]))
+                    );
                 }
+
+                $generator = GeneratorManager::getGenerator($givenGenName);
 
                 if(!$this->getWorlds()->getServer()->generateLevel($args[0], null, $generator)) {
                     $sender->sendMessage($this->getWorlds()->getMessage("create.failed"));
+
+                    return true;
                 }
 
                 $sender->sendMessage($this->getWorlds()->getMessage("create.success", array("name" => $args[0])));
