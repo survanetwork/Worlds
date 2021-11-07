7c003620925494fc5352ff0216b44de927516be1
Jarne <dev@jarne.rocks>
2021-11-07 12:49:29 +0100

    Removed NBT display name renaming when copying/renaming worlds, isn't that important but complex with different world formats

--- src/surva/worlds/commands/CopyCommand.php
+++ src/surva/worlds/commands/CopyCommand.php
@@ -7,8 +7,6 @@ namespace surva\worlds\commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\nbt\BigEndianNBTStream;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\StringTag;
 use surva\worlds\logic\WorldActions;
 
 class CopyCommand extends CustomCommand
@@ -39,41 +37,6 @@ class CopyCommand extends CustomCommand
         $toPath   = $this->getWorlds()->getServer()->getDataPath() . "worlds/" . $toFolderName;
 
         $this->getWorlds()->copy($fromPath, $toPath);
-
-        if (!($levelDatContent = file_get_contents($toPath . "/level.dat"))) {
-            $sender->sendMessage($this->getWorlds()->getMessage("copy.datfile.notexist"));
-
-            return true;
-        }
-
-        $nbt       = new BigEndianNBTStream();
-        $levelData = $nbt->readCompressed($levelDatContent);
-
-        if (!($levelData instanceof CompoundTag) or !$levelData->hasTag("Data", CompoundTag::class)) {
-            $sender->sendMessage($this->getWorlds()->getMessage("copy.datfile.damaged"));
-
-            return true;
-        }
-
-        $dataWorkingWith = $levelData->getCompoundTag("Data");
-
-        if (!$dataWorkingWith->hasTag("LevelName", StringTag::class)) {
-            $sender->sendMessage($this->getWorlds()->getMessage("copy.datfile.damaged"));
-
-            return true;
-        }
-
-        $dataWorkingWith->setString("LevelName", $toFolderName);
-
-        if (!(file_put_contents(
-          $toPath . "/level.dat",
-          $nbt->writeCompressed(new CompoundTag("", [$dataWorkingWith]))
-        ))
-        ) {
-            $sender->sendMessage($this->getWorlds()->getMessage("copy.datfile.notsave"));
-
-            return true;
-        }
 
         $sender->sendMessage(
           $this->getWorlds()->getMessage("copy.success", ["name" => $fromFolderName, "to" => $toFolderName])
--- src/surva/worlds/commands/RenameCommand.php
+++ src/surva/worlds/commands/RenameCommand.php
@@ -7,8 +7,6 @@ namespace surva\worlds\commands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\nbt\BigEndianNBTStream;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\StringTag;
 use surva\worlds\logic\WorldActions;
 
 class RenameCommand extends CustomCommand
@@ -48,41 +46,6 @@ class RenameCommand extends CustomCommand
 
         $fromPath = $this->getWorlds()->getServer()->getDataPath() . "worlds/" . $fromFolderName;
         $toPath   = $this->getWorlds()->getServer()->getDataPath() . "worlds/" . $toFolderName;
-
-        if (!($levelDatContent = file_get_contents($fromPath . "/level.dat"))) {
-            $sender->sendMessage($this->getWorlds()->getMessage("rename.datfile.notexist"));
-
-            return true;
-        }
-
-        $nbt       = new BigEndianNBTStream();
-        $levelData = $nbt->readCompressed($levelDatContent);
-
-        if (!($levelData instanceof CompoundTag) or !$levelData->hasTag("Data", CompoundTag::class)) {
-            $sender->sendMessage($this->getWorlds()->getMessage("rename.datfile.damaged"));
-
-            return true;
-        }
-
-        $dataWorkingWith = $levelData->getCompoundTag("Data");
-
-        if (!$dataWorkingWith->hasTag("LevelName", StringTag::class)) {
-            $sender->sendMessage($this->getWorlds()->getMessage("rename.datfile.damaged"));
-
-            return true;
-        }
-
-        $dataWorkingWith->setString("LevelName", $toFolderName);
-
-        if (!(file_put_contents(
-          $fromPath . "/level.dat",
-          $nbt->writeCompressed(new CompoundTag("", [$dataWorkingWith]))
-        ))
-        ) {
-            $sender->sendMessage($this->getWorlds()->getMessage("rename.datfile.notsave"));
-
-            return true;
-        }
 
         $this->getWorlds()->copy($fromPath, $toPath);
         $this->getWorlds()->delete($fromPath);
